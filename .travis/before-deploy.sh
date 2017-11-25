40956253f7cb461a6aefe8bbde0f98f67a3c7284
☕️ Thoughts <banks@free.fr>
2017-11-25 01:02:13 +0100

    Travis with deploy (#1)
    chore(pom): 🔨 deploy from travis only master/develop
    
    sign artefacts and deploy from travis with escape chars on secrets
    add condition on branch name

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_a4fbf60a1a6d_key -iv $encrypted_a4fbf60a1a6d_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-    gpg --batch --fast-import cd/codesigning.asc
+if ([ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]) && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_a4fbf60a1a6d_key -iv $encrypted_a4fbf60a1a6d_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    gpg --batch --fast-import .travis/codesigning.asc
 fi
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -e -P sign,build-extras --settings cd/mvnsettings.xml
+if ([ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]) && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy -e -P sign,build-extras --settings .travis/mvnsettings.xml
 fi
